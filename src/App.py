e0a7059b45d2c40a4a0786f9e8884a32319ab386
vidithbalasa <vidith@icloud.com>
2023-04-19 20:04:08 -0700

    Connected kivy app with ros to control arm. Works in sim, haven't tested on robot

--- src/App.py
+++ src/App.py
@@ -3,39 +3,71 @@ from kivy.uix.slider import Slider
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.clock import Clock
 from functools import partial
 
+import rospy
+from sensor_msgs.msg import JointState
 
-class MyApp(App):
-	sliders = ['joint2_to_joint1'] + [str(x) for x in range(2,7)]
+class SarBotApp(App):
+    sliders = ["joint2_to_joint1", "joint3_to_joint2", "joint4_to_joint3", 
+            "joint5_to_joint4", "joint6_to_joint5", "joint6output_to_joint6"]
+    default_value = 0.0
+    max_value = 3
+    min_value = -3
+    refresh_rate = 0.1 #seconds
 
-	def build(self):
-		layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
+    prev_joint_positions = [0] * 6
 
-		# Create 6 sliders
-		for slider_name in self.sliders:
-			slider = Slider(min=-3, max=3, value=1)
-			slider_label = Label()
-			self.update_value_label(slider_name, slider_label, slider, slider.value)
+    def build(self):
+        # Initialize ROS node and publisher
+        rospy.init_node('kivy_joint_states_publisher', anonymous=True)
+        self.pub = rospy.Publisher('/joint_states', JointState, queue_size=10)
 
-			layout.add_widget(slider_label)
-			layout.add_widget(slider)
+        layout = BoxLayout(orientation='vertical', spacing=10, padding=10)
 
-			slider.bind(value=partial(self.update_value_label, slider_name, slider_label))
+        # Create 6 sliders
+        self.slider_widgets = []
+        for slider_name in self.sliders:
+            slider = Slider(min=self.min_value, max=self.max_value, value=self.default_value)
+            slider_label = Label()
+            self.update_value_label(slider_name, slider_label, slider, slider.value)
 
-		reset_button = Button(text='Reset', size_hint=(1, 2.5))
-		reset_button.bind(on_press=self.reset_sliders)
-		layout.add_widget(reset_button)
+            layout.add_widget(slider_label)
+            layout.add_widget(slider)
 
-		return layout
+            slider.bind(value=partial(self.update_value_label, slider_name, slider_label))
+            self.slider_widgets.append(slider)
 
-	def reset_sliders(self, instance):
-		for slider in self.root.children[:-1]:
-			slider.value = 1
+        reset_button = Button(text='Reset', size_hint=(1, 2.5))
+        reset_button.bind(on_press=self.reset_sliders)
+        layout.add_widget(reset_button)
 
-	def update_value_label(self, slider_name, value_label, slider, value):
-		value_label.text = f'{slider_name}          Value: {value:.2f}'
+        self.publish_joint_states()
 
+        Clock.schedule_interval(lambda dt: self.publish_joint_states(), self.refresh_rate)
+
+        return layout
+
+    def reset_sliders(self, instance):
+        for slider in self.root.children[:-1]:
+            slider.value = self.default_value
+
+    def update_value_label(self, slider_name, value_label, slider, value):
+        value_label.text = f'{slider_name}          Value: {value:.2f}'
+        self.publish_joint_states()
+
+    def publish_joint_states(self):
+        joint_state = JointState()
+        joint_state.header.stamp = rospy.Time.now()
+        joint_state.name = self.sliders
+        joint_state.velocity = [round(abs(x.value-y)/self.refresh_rate, 2) for x,y in zip(self.slider_widgets, self.prev_joint_positions)]
+        joint_state.position = [slider.value for slider in self.slider_widgets]
+        self.prev_joint_positions = joint_state.position
+        joint_state.effort = []
+
+        rospy.loginfo(joint_state)
+        self.pub.publish(joint_state)
 
 if __name__ == '__main__':
-    MyApp().run()
+    SarBotApp().run()
